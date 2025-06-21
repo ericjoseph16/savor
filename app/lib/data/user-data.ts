d0a98c5fde4735808c903e2035f25a6deaa55d4d
joseps <sajeev@oeware.com>
2025-06-21 12:45:37 -0500

    Added user entity

---
+++ app/lib/data/user-data.ts
@@ -0,0 +1,13 @@
+export type User = {
+  PK: string;    
+  SK: string;
+  userId: string;
+  name: string;
+  email: string;
+  crByNm: string; //Created By Name
+  crDtTm: string; //Created By Date Time
+  upBy: string;   //Updated By
+  upByNm: string; //Updated By Name
+  upDtTm: string; //Updated By Date Time
+  et: string;     //Entity Type
+}
