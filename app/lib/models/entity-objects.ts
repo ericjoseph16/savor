0b029657f04ec36b01e99b069d710ea7a79b18e1
ericjoseph16 <ericjoseph16@gmail.com>
2025-07-16 16:15:12 -0400

    Added User Food Preference Entity

--- app/lib/models/entity-objects.ts
+++ app/lib/models/entity-objects.ts
@@ -1,4 +1,4 @@
-import { UserRole } from "./helper-objects";
+import { UserRole, FoodPreference} from "./helper-objects";
 
 /**
  * User entity
@@ -17,6 +17,30 @@ export type User = {
   name: string;
   email: string;
   role: UserRole;
+  crById: string;
+  crByName: string;
+  crDtTm: string;
+  updById: string;
+  updByName: string;
+  updDtTm: string;
+  entityType: string;
+}
+
+// User Food Preference Entity
+export type UserFoodPreference = {
+  PK: string;
+  SK: string;
+  GSI1PK: string;
+  GSI1SK: string;
+  // GSI2PK: string;
+  // GSI2SK: string;
+  // GSI3PK: string;
+  // GSI3SK: string;
+  userId: string;
+  userFoodPreferenceId: string;
+  mealId: string;
+  foodName: string;
+  preference: FoodPreference;
   crById: string;
   crByName: string;
   crDtTm: string;
